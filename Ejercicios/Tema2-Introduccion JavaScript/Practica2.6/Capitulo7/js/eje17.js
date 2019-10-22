ac36286c17adc654f83fdc38be66aaad58b0d303
cralr <rafamcruza10@gmail.com>
2019-10-22 17:27:55 +0200

    Subida practica2.6

---
+++ Ejercicios/Tema2-Introduccion JavaScript/Practica2.6/Capitulo7/js/eje17.js
@@ -0,0 +1,26 @@
+/**
+ * Mejorar el ejemplo anterior indicando en todo momento al usuario el número de caracteres que aún puede escribir.
+ * Además, se debe permitir pulsar las teclas Backspace, Supr. y las flechas horizontales cuando se haya llegado al máximo número de caracteres.
+ * 
+ * @author Rafael Miguel Cruz Álvarez
+ */
+
+{
+    let elemento;
+
+    let inicio = function(){
+        elemento = document.getElementById("texto");
+
+    }
+
+    window.onload = inicio;
+
+    let limita = function(event){
+
+        
+    }
+
+
+
+
+}
