b63f78300e930f1bdfc1ed706fb6b7fb8448e1f5
cralr <rafamcruza10@gmail.com>
2019-10-23 12:59:33 +0200

    Subida eje16.js corregido

--- Ejercicios/Tema2-Introduccion JavaScript/Practica2.5/Capitulo6/js/eje16.js
+++ Ejercicios/Tema2-Introduccion JavaScript/Practica2.5/Capitulo6/js/eje16.js
@@ -7,48 +7,37 @@
 */
 
 {
+    let elementoDiv;
 
-    let informacion = function (event) {
-        let evento = event;
-
+    let informacion = function (evento) {
         let coordenadaX = evento.clientX;
         let coordenadaY = evento.clientY;
 
-        let dimensiones = tamanoVentanaNavegador();
-        let tamanoX = dimensiones[0];
-        let tamanoY = dimensiones[1];
+        let [tamanoX,tamanoY] = tamanoVentanaNavegador();
 
-        let posicionHorizontal = "";
-        let posicionVertical = "";
-
-        if (coordenadaX > tamanoX / 2) {
-            posicionHorizontal = "derecha";
-        }
-        else {
-            posicionHorizontal = "izquierda";
-        }
-
-        if (coordenadaY > tamanoY / 2) {
-            posicionVertical = "abajo";
-        }
-        else {
-            posicionVertical = "arriba";
-        }
+        let posicionHorizontal =(coordenadaX > tamanoX/2) ? "Derecha" : "Izquierda";
+        let posicionVertical = (coordenadaY > tamanoY/2) ? "Abajo" : "Arriba";
 
         muestraInformacion(['Posición', posicionHorizontal, posicionVertical]);
     }
 
     let muestraInformacion = function (mensaje) {
-        document.getElementById("info").innerHTML = '<h1>' + mensaje[0] + '</h1>';
+        elementoDiv.innerHTML = '<h1>' + mensaje[0] + '</h1>';
         for (let i = 1; i < mensaje.length; i++) {
-            document.getElementById("info").innerHTML += '<p>' + mensaje[i] + '</p>';
+          elementoDiv.innerHTML += '<p>' + mensaje[i] + '</p>';
         }
     }
+
     let tamanoVentanaNavegador = function () {
-        let dimensiones = [];
-        dimensiones = [window.innerWidth, window.innerHeight];
-        return dimensiones;
+        return [window.innerWidth, window.innerHeight];
     }
 
-     document.onclick = informacion;
+    let init = function(){
+        elementoDiv = document.getElementById("info");
+        
+        document.addEventListener("click",informacion);
+    }
+
+    document.addEventListener("DOMContentLoaded",init);
 }
+    
